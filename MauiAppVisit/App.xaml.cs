3247f68cbef01433b47bfbbd2f92a3ea875b169d
leonardo reis <leonardo.amorim@gomara.tech>
2023-09-20 13:58:39 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppVisit/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppVisit
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
