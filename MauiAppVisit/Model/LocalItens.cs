712165fcfed805f26048ea1d0629687bb0d1b851
leonardo reis <leonardo.amorim@gomara.tech>
2023-09-20 14:20:13 -0300

    create models

---
+++ MauiAppVisit/Model/LocalItens.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppVisit.Model
+{
+    public class LocalItens
+    {
+        public int Id { get; set; }
+        public string PlaceName { get; set; }
+        public string ImagePlace { get; set; }
+    }
+}
