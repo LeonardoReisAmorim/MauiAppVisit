ef696213d7b1789a6bdb1fca0fad64246c2ffebf
Leonardo Amorim <leonardo.amorim@gomara.tech>
2023-09-21 00:25:27 -0300

    layout details finalizado

--- MauiAppVisit/App.xaml.cs
+++ MauiAppVisit/App.xaml.cs
@@ -5,8 +5,11 @@
         public App()
         {
             InitializeComponent();
-
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new MainPage())
+            {
+                BarBackgroundColor = Color.FromRgb(16, 181, 191),
+                BarTextColor = Color.FromRgb(255, 255, 255),
+            };
         }
     }
 }
