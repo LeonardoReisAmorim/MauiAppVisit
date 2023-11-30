cb4617de0d2bb3d6325c97e21086019ea01d102e
Leonardo Amorim <leonardo.amorim@gomara.tech>
2023-11-30 14:07:44 -0300

    refatoracao

--- MauiAppVisit/ViewModel/LocationDetailsViewModel.cs
+++ MauiAppVisit/ViewModel/LocationDetailsViewModel.cs
@@ -69,9 +69,6 @@ namespace MauiAppVisit.ViewModel
         private async Task GetArquivoAsync()
         {
             Loading = "true";
-            var arquivoBytesZip = Array.Empty<byte>();
-            var arquivoZip = new ArquivoDTO();
-            var filenameApk = string.Empty;
             var baseUrl = HttpHelper.GetBaseUrl();
             var htppClient = HttpHelper.GetHttpClient();
 
@@ -80,60 +77,30 @@ namespace MauiAppVisit.ViewModel
 
             if (response.IsSuccessStatusCode)
             {
-                var responseContent = await response.Content.ReadAsStreamAsync();
+                var responseContent = response.Content.ReadAsStream();
 
                 using (var arquivos = new ZipArchive(responseContent, ZipArchiveMode.Read))
                 {
-                    foreach (ZipArchiveEntry arquivoApk in arquivos.Entries)
-                    {
-                        var streamAPK = arquivoApk.Open();
-                        filenameApk = arquivoApk.Name;
 
-                        // this will run for Android 33 and greater
-                        if (DeviceInfo.Platform == DevicePlatform.Android && OperatingSystem.IsAndroidVersionAtLeast(33))
-                        {
-#if ANDROID
+                    // this will run for Android 33 and greater
+                    if (DeviceInfo.Platform == DevicePlatform.Android && OperatingSystem.IsAndroidVersionAtLeast(33))
+                    {
+                    #if ANDROID
                             var activity = Platform.CurrentActivity ?? throw new NullReferenceException("Current activity is null");
                             if (ContextCompat.CheckSelfPermission(activity, Manifest.Permission.ReadExternalStorage) != Permission.Granted)
                             {
                                 ActivityCompat.RequestPermissions(activity, new[] { Manifest.Permission.ReadExternalStorage }, 1);
                             }
-#endif
-                        }
-
-                        await FileSaver.Default.SaveAsync(filenameApk, streamAPK, new CancellationToken());
+                    #endif
                     }
+
+                    var arquivoApk = arquivos.Entries[0];
+                    var streamAPK = arquivoApk.Open();
+
+                    await FileSaver.Default.SaveAsync(arquivoApk.Name, streamAPK, new CancellationToken());
                 }
 
             }
-            //arquivoBytesZip = Convert.FromBase64String(arquivoZip.arquivo);
-
-            //using (MemoryStream msZip = new MemoryStream(arquivoBytesZip))
-            //{
-            //    using (var arquivos = new ZipArchive(msZip, ZipArchiveMode.Read))
-            //    {
-            //        foreach (ZipArchiveEntry arquivoApk in arquivos.Entries)
-            //        {
-            //            var streamAPK = arquivoApk.Open();
-            //            filenameApk = arquivoApk.Name;
-
-            //            // this will run for Android 33 and greater
-            //            if (DeviceInfo.Platform == DevicePlatform.Android && OperatingSystem.IsAndroidVersionAtLeast(33))
-            //            {
-            //                #if ANDROID
-            //                    var activity = Platform.CurrentActivity ?? throw new NullReferenceException("Current activity is null");
-            //                    if (ContextCompat.CheckSelfPermission(activity, Manifest.Permission.ReadExternalStorage) != Permission.Granted)
-            //                    {
-            //                        ActivityCompat.RequestPermissions(activity, new[] { Manifest.Permission.ReadExternalStorage }, 1);
-            //                    }
-            //                #endif
-            //            }
-
-            //            await FileSaver.Default.SaveAsync(filenameApk, streamAPK, new CancellationToken());
-            //        }
-            //    }
-            //}
-
             Loading = "false";
             Aviso = "Após feita instalação do ambiente virtual em formato .apk. Serão necessários alguns passos:\n1 - Instalar o arquivo .apk referente ao ambiente virtual\n2 - Se divirta!";
         }
